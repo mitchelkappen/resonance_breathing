5ec353c91f4ef9ba124edbce0a21ba5517051bff
XanderCornelis <Xander.c2000@gmail.com>
2022-12-16 14:04:44 +0100

    Update 1.1_SelfReport.R
    Adjustments 16_12

--- scripts/1.1_SelfReport.R
+++ scripts/1.1_SelfReport.R
@@ -14,6 +14,7 @@
 # install.packages("multcompView")
 # install.packages("ggthemes")
 # install.packages("ggsignif")
+# install.packages("papaja")
 
 library(car)
 library(lme4)
@@ -28,11 +29,14 @@ library(egg)
 library(multcompView)
 library(ggthemes) 
 library(ggsignif)
+library(papaja)
 
 ##### Set environment #####
 rm(list = ls()) # Clear environment
 cat("\014") # Clear console # # Or ctrl + l in VSCode
 dev.off() # Clear plot window
+
+cbPalette <- c("#F0E442", "#0072B2", "#D55E00") # Define Colorblind proof plotting colors
 
 # Set and Get directories
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) #Set WD to script location
@@ -42,9 +46,21 @@ options(contrasts = c("contr.sum","contr.poly")) # Use this for the p value of t
 nAGQ = 1
 plotPrefix <- "/../figures/"
 
+# function for plot
+plotfunction <-
+  function(emmean_dataframe, title){
+    ggplot(emmean_dataframe, aes(x=Phase, y=emmean, colour = Phase)) +
+      geom_point(size = 5) + 
+      geom_line(aes(group = 1),size = 1, colour = "black", linetype = "dotted")+
+      geom_errorbar(width=.25, size = 1.4, aes(ymin=emmean-SE, ymax=emmean+SE))+
+      labs(y = title, x = "Phase")+
+      scale_colour_manual(values=cbPalette)+
+      theme_apa()
+  }
+
 ##### Loading data ##### 
 # Reading in data
-data <- read_delim("../loc_data/data_SF.txt")
+data <- read_delim("data_SF.txt")
 data$tDCSGroup[data$tDCSGroup == 'Active '] = 'Active'
 data$tDCSGroup[data$tDCSGroup == 'Sham '] = 'Sham'
 # Remove unnecessary columns
@@ -70,6 +86,7 @@ densityPlot(data$AF_SoothingPositiveAffect)
 
 summary(data) # I like to do this because it is easy to check the variable types
 
+
 ####### Stats #######
 
 # 1) Negative Affect ######
@@ -79,8 +96,6 @@ dataModel = data # Ensure correct data is taken
 rm(d0.1, d0.2, d0.3, tabel, chosenModel, emmeans0.1, emmeans0.2, emm0.1, figure) # Just to be sure you're not comparing former models for this comparison
 
 d0.1 <- lmer(formula,data=dataModel)
-# d0.2 <- glmer(formula,data=dataModel, family = Gamma(link = "identity"),glmerControl(optimizer= "bobyqa", optCtrl = list(maxfun = 100000)),nAGQ = nAGQ)
-# d0.3 <- glmer(formula,data=dataModel, family = inverse.gaussian(link = "identity"),glmerControl(optimizer= "bobyqa", optCtrl = list(maxfun = 100000)),nAGQ = nAGQ)
 
 modelNames = c(d0.1)
 tabel <- cbind(AIC(d0.1))
@@ -89,7 +104,13 @@ chosenModel = modelNames[which(tabel == min(tabel))] # Get model with lowest AIC
 Anova(chosenModel[[1]], type = 'III')
 plot(effect("Phase:tDCSGroup:Breathing_Condition", chosenModel[[1]])) # Visualize the three way interaction we are interested in
 
-emmeans(chosenModel[[1]], pairwise ~ Phase, adjust ="fdr", type = "response") # Pairwise comparisons
+emmeans0.1<- emmeans(chosenModel[[1]], pairwise ~ Phase, adjust ="fdr", type = "response") # Pairwise comparisons
+emm0.1 <- summary(emmeans0.1)$emmeans
+emmeans0.1$contrasts
+
+NegAffectPlot<- plotfunction(emm0.1, "Negative Affect")
+NegAffectPlot<- NegAffectPlot + annotate('text', x=1.5, y=mean(emm0.1$emmean) + (max(emm0.1$emmean) - min(emm0.1$emmean)) / 2, label='', size=7)
+NegAffectPlot 
 
 # 2) AF_ActivatingPositiveAffect ######
 formula <- 'AF_ActivatingPositiveAffect ~ Phase*tDCSGroup*Breathing_Condition + (1|Subject)' # Declare formula
@@ -108,7 +129,13 @@ chosenModel = modelNames[which(tabel == min(tabel))] # Get model with lowest AIC
 Anova(chosenModel[[1]], type = 'III')
 plot(effect("Phase:tDCSGroup:Breathing_Condition", chosenModel[[1]])) # Visualize the three way interaction we are interested in
 
-emmeans(chosenModel[[1]], pairwise ~ Phase, adjust ="fdr", type = "response") # Pairwise comparisons
+emmeans0.1<- emmeans(chosenModel[[1]], pairwise ~ Phase, adjust ="fdr", type = "response") # Pairwise comparisons
+emm0.1 <- summary(emmeans0.1)$emmeans
+emmeans0.1$contrasts
+
+ActPositivePlot<- plotfunction(emm0.1, "Activating Positive Affect")
+ActPositivePlot<- ActPositivePlot + annotate('text', x=1.5, y=mean(emm0.1$emmean) + (max(emm0.1$emmean) - min(emm0.1$emmean)) / 2, label='', size=7)
+ActPositivePlot
 
 # 3) AF_SoothingPositiveAffect ######
 formula <- 'AF_SoothingPositiveAffect ~ Phase*tDCSGroup*Breathing_Condition + (1|Subject)' # Declare formula
@@ -127,62 +154,12 @@ chosenModel = modelNames[which(tabel == min(tabel))] # Get model with lowest AIC
 Anova(chosenModel[[1]], type = 'III')
 plot(effect("Phase:tDCSGroup:Breathing_Condition", chosenModel[[1]])) # Visualize the three way interaction we are interested in
 
-emmeans(chosenModel[[1]], pairwise ~ Phase, adjust ="fdr", type = "response")# Pairwise comparisons
+emmeans0.1<- emmeans(chosenModel[[1]], pairwise ~ Phase, adjust ="fdr", type = "response") # Pairwise comparisons
+emm0.1 <- summary(emmeans0.1)$emmeans
+emmeans0.1$contrasts
 
+SootPositivePlot<- plotfunction(emm0.1, "Soothing Positive Affect")
+SootPositivePlot<- SootPositivePlot + annotate('text', x=1.5, y=mean(emm0.1$emmean) + (max(emm0.1$emmean) - min(emm0.1$emmean)) / 2, label='', size=7)
+SootPositivePlot
 
-####### plots #######
-
-
-# 1) AF_NegativeAffect
-
-# Table with the mean and the standard deviation for every combination
-dt1 <- group_by(data, Phase, Breathing_Condition, tDCSGroup) %>%
-  summarise(NegAffect_mean=mean(AF_NegativeAffect), sd=sd(AF_NegativeAffect)) %>%
-  arrange(desc(NegAffect_mean))
-dt1
-
-# Barplot based on dt
-ggplot(dt1, aes(x = Phase, y = NegAffect_mean, fill = tDCSGroup:Breathing_Condition)) +
-  geom_bar(stat = "identity", position = "dodge") +
-  # geom_errorbar(aes(ymax = NegAffect_mean + sd, ymin = NegAffect_mean - sd),
-  #               position = position_dodge(0.9), width = 0.25, color = "Gray25") + # Include errorbar
-  geom_signif(comparisons = list(c("Stress", "Breathing")), map_signif_level=T) +  # Include significance lines
-  geom_signif(comparisons = list(c("Habituation", "Stress")), map_signif_level=T) +  # Include significance lines
-  scale_fill_brewer(palette = "Greens") +
-  theme_few()
-
-# 2) AF_ActivatingPositiveAffect
-
-# Table with the mean and the standard deviation for every combination
-dt2 <- group_by(data, Phase, Breathing_Condition, tDCSGroup) %>%
-  summarise(APosAffect_mean=mean(AF_ActivatingPositiveAffect), sd=sd(AF_ActivatingPositiveAffect)) %>%
-  arrange(desc(APosAffect_mean))
-dt2
-
-# Barplot based on dt
-ggplot(dt2, aes(x = Phase, y = APosAffect_mean, fill = tDCSGroup:Breathing_Condition)) +
-  geom_bar(stat = "identity", position = "dodge") +
-  # geom_errorbar(aes(ymax = APosAffect_mean + sd, ymin = APosAffect_mean - sd),
-  #               position = position_dodge(0.9), width = 0.25, color = "Gray25") + # Include errorbar
- geom_signif(comparisons = list(c("Habituation", "Breathing")), map_signif_level=T) +  # Include significance lines
-  scale_fill_brewer(palette = "Greens") +
-  theme_few()
-
-# 3) AF_SoothingPositiveAffect
-
-# Table with the mean and the standard deviation for every combination
-dt3 <- group_by(data, Phase, Breathing_Condition, tDCSGroup) %>%
-  summarise(SPosAffect_mean=mean(AF_SoothingPositiveAffect), sd=sd(AF_SoothingPositiveAffect)) %>%
-  arrange(desc(SPosAffect_mean))
-dt3
-
-# Barplot based on dt
-ggplot(dt3, aes(x = Phase, y = SPosAffect_mean, fill = tDCSGroup:Breathing_Condition)) +
-  geom_bar(stat = "identity", position = "dodge") +
-  # geom_errorbar(aes(ymax = SPosAffect_mean + sd, ymin = SPosAffect_mean - sd),
-  #               position = position_dodge(0.9), width = 0.25, color = "Gray25") + # Include errorbar
-  geom_signif(comparisons = list(c("Stress", "Breathing")), map_signif_level=T) +   # Include significance lines
-  geom_signif(comparisons = list(c("Habituation", "Stress")), map_signif_level=T) +  # Include significance lines
-  scale_fill_brewer(palette = "Greens") +
-  theme_few()
 
